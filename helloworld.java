95b0986542a31e3244805c69b507e535dd97e536
maheshperabathula <95281362+maheshperabathula@users.noreply.github.com>
2022-01-05 18:50:28 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
-public class HelloWorld.java {
+public class HelloWorld {
 	  public static void main(String[] args) {
 	    System.out.println("Haapy sankranthi");
-             System.out.println("Haapy sankranthi");   
-	  }
-	}
+             System.out.println("Haapy sankranthi");
+}
+}
